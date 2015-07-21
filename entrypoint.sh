b6e41263bf1d41a2f6e08c0db1698b561e7affb7
Sameer Naik <sameer@damagehead.com>
2015-07-21 22:51:43 +0530

    entrypoint.sh: cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,6 @@ set -e
 mkdir -p ${MONGO_DATA_DIR}
 chmod -R 0755 ${MONGO_DATA_DIR}
 chown -R ${MONGO_USER}:${MONGO_USER} ${MONGO_DATA_DIR}
-exec start-stop-daemon --start --chuid ${MONGO_USER}:${MONGO_USER} --exec /usr/bin/mongod -- --config /etc/mongod.conf --httpinterface --rest
+
+exec start-stop-daemon --start --chuid ${MONGO_USER}:${MONGO_USER} \
+  --exec /usr/bin/mongod -- --config /etc/mongod.conf --httpinterface --rest
