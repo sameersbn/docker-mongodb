de440ab5d0cee59745437986d3b8db089dd29e1a
Sameer Naik <sameer@damagehead.com>
2015-07-21 22:55:09 +0530

    entrypoint.sh: allow users to specify arguments to `mongod`

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,19 @@ mkdir -p ${MONGO_DATA_DIR}
 chmod -R 0755 ${MONGO_DATA_DIR}
 chown -R ${MONGO_USER}:${MONGO_USER} ${MONGO_DATA_DIR}
 
+# allow arguments to be passed to mongod
+if [[ ${1:0:1} = '-' ]]; then
+  EXTRA_ARGS="$@"
+  set --
+elif [[ ${1} == mongod || ${1} == $(which mongod) ]]; then
+  EXTRA_ARGS="${@:2}"
+  set --
+fi
+
 # default behaviour is to launch mongod
 if [[ -z ${1} ]]; then
   exec start-stop-daemon --start --chuid ${MONGO_USER}:${MONGO_USER} \
-    --exec $(which mongod) -- --config /etc/mongod.conf --httpinterface --rest
+    --exec $(which mongod) -- --config /etc/mongod.conf --httpinterface --rest ${EXTRA_ARGS}
 else
   exec "$@"
 fi
