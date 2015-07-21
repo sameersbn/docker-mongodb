69c0cff967cb350417512adb7b4cf340cfaba529
Sameer Naik <sameer@damagehead.com>
2015-07-21 22:53:54 +0530

    entrypoint.sh: allow users to specify the command

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,10 @@ mkdir -p ${MONGO_DATA_DIR}
 chmod -R 0755 ${MONGO_DATA_DIR}
 chown -R ${MONGO_USER}:${MONGO_USER} ${MONGO_DATA_DIR}
 
-exec start-stop-daemon --start --chuid ${MONGO_USER}:${MONGO_USER} \
-  --exec /usr/bin/mongod -- --config /etc/mongod.conf --httpinterface --rest
+# default behaviour is to launch mongod
+if [[ -z ${1} ]]; then
+  exec start-stop-daemon --start --chuid ${MONGO_USER}:${MONGO_USER} \
+    --exec $(which mongod) -- --config /etc/mongod.conf --httpinterface --rest
+else
+  exec "$@"
+fi
