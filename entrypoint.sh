d3a8b3744aba3ea06330683b2658debf1b0841f2
Sameer Naik <sameer@damagehead.com>
2015-07-21 22:51:28 +0530

    entrypoint.sh: create `MONGO_DATA_DIR` directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
+# create data dir
+mkdir -p ${MONGO_DATA_DIR}
+chmod -R 0755 ${MONGO_DATA_DIR}
 chown -R ${MONGO_USER}:${MONGO_USER} ${MONGO_DATA_DIR}
 exec start-stop-daemon --start --chuid ${MONGO_USER}:${MONGO_USER} --exec /usr/bin/mongod -- --config /etc/mongod.conf --httpinterface --rest
