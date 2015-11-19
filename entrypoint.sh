0ee6ffdd46476efa6c97ce5a723e1ffc5b9cdccc
Laurens Rietveld <laurens.rietveld@gmail.com>
2015-11-19 12:06:01 +0100

    added UID and GID mapping

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
+map_uidgid() {
+  USERMAP_ORIG_UID=$(id -u ${MONGO_USER})
+  USERMAP_ORIG_GID=$(id -g ${MONGO_USER})
+  USERMAP_GID=${USERMAP_GID:-${USERMAP_GID:-$USERMAP_ORIG_GID}}
+  USERMAP_UID=${USERMAP_UID:-$USERMAP_ORIG_UID}
+  if [[ ${USERMAP_UID} != ${USERMAP_ORIG_UID} ]] || [[ ${USERMAP_GID} != ${USERMAP_ORIG_GID} ]]; then
+    echo "Mapping UID and GID for ${MONGO_USER}:${MONGO_USER} from ${USERMAP_ORIG_UID}:${USERMAP_ORIG_GID} to ${USERMAP_UID}:${USERMAP_GID}"
+    groupmod -g ${USERMAP_GID} ${MONGO_USER}
+    sed -ie "s/:${USERMAP_ORIG_UID}:${USERMAP_ORIG_GID}:/:${USERMAP_UID}:${USERMAP_GID}:/" /etc/passwd
+  fi
+}
+
 create_data_dir() {
   mkdir -p ${MONGO_DATA_DIR}
   chmod -R 0755 ${MONGO_DATA_DIR}
@@ -13,6 +25,7 @@ create_log_dir() {
   chown -R ${MONGO_USER}:${MONGO_USER} ${MONGO_LOG_DIR}
 }
 
+map_uidgid
 create_data_dir
 create_log_dir
 
